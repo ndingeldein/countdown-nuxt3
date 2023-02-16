f2c31c12d29a46a0bb9450d94fc5533be2ce0c8b
neil dingeldein <ndingeldein@gmail.com>
2023-02-16 12:50:17 -0600

    adjustments

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full min-h-screen pb-20 bg-purple-800">
+  <div class="w-full min-h-screen pb-20 overflow-hidden bg-purple-800">
     <Countdown />
   </div>
 </template>
